2fbc6ca95890f040c71067251f44b2d8fe921396
Umpoy <iumpoy@yahoo.com>
2018-12-19 15:26:15 -0800

    connectd db with routes

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const mysql = require('mysql');
 const configDB = require('./config/database');
 const db = mysql.createConnection(configDB);
 
+require('./app/routes.js')(app, db);
+
 const port = 5000;
 
 app.listen(port, () => {
