06ee042a82a812715cd58e69bd8bdfcf4e608dc4
Umpoy <iumpoy@yahoo.com>
2018-12-19 15:21:50 -0800

    created connection to db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const mysql = require('mysql');
+const configDB = require('./config/database');
+const db = mysql.createConnection(configDB);
+
 const port = 5000;
 
 app.listen(port, () => {
