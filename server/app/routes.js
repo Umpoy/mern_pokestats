f93ea7117c7986047d8e976dddc48c4c71b861e5
Umpoy <iumpoy@yahoo.com>
2018-12-19 16:40:38 -0800

    pokemon now saved to db

--- server/app/routes.js
+++ server/app/routes.js
@@ -7,14 +7,31 @@ module.exports = (app, db) => {
     });
 
     app.post('/sendToDB', (req, res) => {
-        const { id, name, type, stats } = req.body
+        let { id, name, type, stats } = req.body
+        type = JSON.stringify(type);
+        stats = JSON.stringify(stats);
         db.connect((err) => {
             if (err) {
                 throw err
             }
             console.log("Connected to DB!!!");
-            let sql = "INSERT INTO `Pokemon`(`id`, `name`, `type`, `stats`) VALUES (" + id + "," + name + "," + type + "," + stats + ")";
-            
+            let sql = "SELECT `id` FROM `Pokemon` WHERE `id` = " + id
+            db.query(sql, (err, result, fields) => {
+                if (err) {
+                    throw err
+                }
+                if (result.length > 0) {
+                    console.log('id is taken');
+                } else {
+                    let sql = "INSERT INTO `Pokemon`(`id`, `name`, `type`, `stats`) VALUES (" + id + ",'" + name + "','" + type + "','" + stats + "')";
+                    db.query(sql, (err, result) => {
+                        if (err) {
+                            throw err;
+                        }
+                        console.log("saved to db")
+                    })
+                }
+            });
         });
     })
 }
