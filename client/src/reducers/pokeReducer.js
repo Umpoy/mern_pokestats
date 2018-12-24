b4e76ada57ced562dcf52e05130670593a8d9b8a
Umpoy <iumpoy@yahoo.com>
2018-12-24 10:42:33 -0800

    added SELECT_POKEMON to reducer

--- client/src/reducers/pokeReducer.js
+++ client/src/reducers/pokeReducer.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
     switch (action.type) {
         case "FETCH_POKEMON":
             return action.payload;
+        case "SELECT_POKEMON":
+            return action.payload;
         default:
             return state;
     }
