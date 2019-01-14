1401cbc999925f4436a3a8366eb6caf68d6782cc
Umpoy <iumpoy@yahoo.com>
2019-01-14 12:26:37 -0800

    created PokeForm

--- client/src/components/PokeList.js
+++ client/src/components/PokeList.js
@@ -51,29 +51,7 @@ class PokeList extends Component {
     render() {
         return (
             <div>
-                <form action="get">
-                    <select name="types" defaultValue="all" id="" onChange={this.grabTypes}>
-                        <option value="all">All</option>
-                        <option value="grass">Grass</option>
-                        <option value="poison">Poison</option>
-                        <option value="flying">Flying</option>
-                        <option value="normal">Normal</option>
-                        <option value="fighting">Fighting</option>
-                        <option value="ground">Ground</option>
-                        <option value="rock">Rock</option>
-                        <option value="bug">Bug</option>
-                        <option value="ghost">Ghost</option>
-                        <option value="steel">Steel</option>
-                        <option value="fire">Fire</option>
-                        <option value="water">Water</option>
-                        <option value="electric">Electric</option>
-                        <option value="psychic">Psychic</option>
-                        <option value="ice">Ice</option>
-                        <option value="dragon">Dragon</option>
-                        <option value="dark">Dark</option>
-                        <option value="fairy">Fairy</option>
-                    </select>
-                </form>
+                
                 <div className="ui stackable three column grid">
                     {this.renderList()}
                 </div>
