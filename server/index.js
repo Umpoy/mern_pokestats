18b9769af8daabd733e27cf7b7277b13ab6caff9
Umpoy <iumpoy@yahoo.com>
2019-01-22 15:11:06 -0800

    updating live site

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 const path = require('path');
 const port = 5000;
 
@@ -8,9 +8,9 @@ const mysql = require('mysql');
 const configDB = require('./config/database');
 const db = mysql.createConnection(configDB);
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
-// app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
 require('./app/routes.js')(app, db);
 
